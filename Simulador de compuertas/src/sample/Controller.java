8e44bf8ff3e5801ead43c807fe0243a58d7c0ccc
DaniCamacho29 <danicamacho7@hotmail.com>
2019-09-16 14:45:18 -0600

    Primer Commit

---
+++ Simulador de compuertas/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+        @FXML private VBox VboxCompuertas;
+        @FXML private ImageView And;
+        @FXML private ImageView Or;
+        @FXML private ImageView Nor;
+        @FXML private ImageView Xnor;
+        @FXML private ImageView Xor;
+        @FXML private ImageView Nand;
+        @FXML private ImageView Not;
+        @FXML private ImageView Borrador;
+
+
+}
+
